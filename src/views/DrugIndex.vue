b22a0fde48064aa278fdb955cf4253f979af6a55
erwin <winzon353535@gmail.com>
2022-05-18 13:40:13 -0700

    adds action and create function for DrugIndex.vue

---
+++ src/views/DrugIndex.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "List of All Drugs",
+      drugs: [],
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/drugs.json").then((response) => {
+      this.drugs = response.data;
+      console.log("All Drugs", this.drugs);
+    });
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+
+  <div v-for="drug in drugs" v-bind:key="drug.id">
+    <img :src="drug.image_url" :alt="drug.name" />
+    <p>Name: {{ drug.name }}</p>
+    <p>Description: {{ drug.description }}</p>
+    <p>Classification: {{ drug.classification.name }}</p>
+  </div>
+</template>
