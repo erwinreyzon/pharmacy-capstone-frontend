bb921acf89a376b7bc8f03c1e2fcf69d92290cd2
erwin <winzon353535@gmail.com>
2022-05-20 11:16:44 -0700

    adds created function to GET show drug in DrugShow.vue

---
+++ src/views/DrugShow.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      drug: {},
+    };
+  },
+  created: function () {
+    axios.get("localhost:3000/drugs/" + this.$route.params.id + ".json").then((response) => {
+      console.log("Show Drug", response.data);
+      this.drug = response.data;
+    });
+  },
+};
+</script>
+
+<template></template>
