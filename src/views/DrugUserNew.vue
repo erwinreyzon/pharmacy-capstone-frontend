ea92c30a9eec4b829f4d859ba1d26d3eca1d3837
erwin <winzon353535@gmail.com>
2022-05-31 00:08:18 -0700

    adds function to DrugUserNew.vue

--- src/views/DrugUserNew.vue
+++ src/views/DrugUserNew.vue
@@ -1,3 +1,33 @@
-<script></script>
+<script>
+import axios from "axios";
 
-<template></template>
+export default {
+  data: function () {
+    return {
+      newDrugUserParams: {},
+      errors: [],
+    };
+  },
+  methods: {
+    createDrugUser: function () {
+      console.log("Adding Drug to User");
+
+      axios
+        .post("http://localhost:3000/drug_users.json", this.newDrugUserParams)
+        .then((response) => {
+          console.log("Success", response.data);
+          this.drug_users.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="druguser-new">
+    <form v-on:submit.prevent="createDrugUser()">
+      <h1>Please Input Information</h1>
+    </form>
+  </div>
+</template>
